77ec2feab9539b386ede3fcf672dfc1f7794257a
didierClin <didier.c62@numericable.fr>
2017-01-14 14:29:45 +0100

    moved pawns definition at first - gets team array with number and names of players

--- game.rb
+++ game.rb
@@ -1,3 +1,7 @@
+#created 4 pawns in the shape of animals
+# Sorry JR I change my point of view ;)
+pawns =["rabbit", "auk", "fox" , "bear"]
+
 # Thommy
 #game board - master piece of program (for me ;) )
 #change vars depart, pont and arriver (ee) to hash of zones
@@ -10,22 +14,19 @@
 #
 # #Zone d'arriver
 # arriver = "iglou"
+# John
 
-zones = { ice_field: [pawns[0],pawns[1],pawns[2],pawns[3] , bridge: [] , snow_house: [] }
+zones = { ice_field: [pawns[0],pawns[1],pawns[2],pawns[3]] , bridge: [] , snow_house: [] }
 
 
 # Dana
 dice  = ["bridge","snow_house","ice_cube","bridge","snow_house","ice_cube"]
 
 
-# John
-#created 4 pawns in the shape of animals
-# Sorry JR I change my point of view ;)
-pawns =["rabbit", "auk", "fox" , "bear"]
 
 # The dice
 # Dice roll
-puts dice_roll
+# puts dice_roll
 
 
 # Didier
@@ -35,7 +36,11 @@ bridge = 6
 
 # many players but one team
 puts "How many players in the team : ?"
-team = gets.to_i
+team = []
+(1..gets.to_i).each do |p|
+  puts "What's your name player #{p}"
+  team << gets.chomp
+end
 
 
 
