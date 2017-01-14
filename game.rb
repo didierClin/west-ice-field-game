900be74fd4106c6af0289d1d9b678c2a569b0a39
didierClin <didier.c62@numericable.fr>
2017-01-14 15:06:02 +0100

    add throw dice and if statement with modulo for action

--- game.rb
+++ game.rb
@@ -1,38 +1,19 @@
-#created 4 pawns in the shape of animals
-# Sorry JR I change my point of view ;)
+# John
+#create 4 pawns in the shape of animals
 pawns =["rabbit", "auk", "fox" , "bear"]
 
 # Thommy
 #game board - master piece of program (for me ;) )
 #change vars depart, pont and arriver (ee) to hash of zones
-# zones = { depart pont arrivee }
-#
-# depart = "zone de pêche"
-#
-# #Pont
-# pont = "transition"
-#
-# #Zone d'arriver
-# arriver = "iglou"
-# John
-
 zones = { ice_field: [pawns[0],pawns[1],pawns[2],pawns[3]] , bridge: [] , snow_house: [] }
 
-
 # Dana
+# create dice
 dice  = ["bridge","snow_house","ice_cube","bridge","snow_house","ice_cube"]
-
-
-
-# The dice
-# Dice roll
-# puts dice_roll
-
 
 # Didier
 # Create bridge with 6 pillar
 bridge = 6
-
 
 # many players but one team
 puts "How many players in the team : ?"
@@ -42,7 +23,23 @@ team = []
   team << gets.chomp
 end
 
-
+#Ready to play?
+puts "Let's start \n"
+# Game ends when bridge has no pillar or when all pawns are safe on snow-house land
+win = false
+while ( bridge != 0 || win == true ) do
+  # we calculate result of a dice(6) throw modulo 3
+  throw = (rand(6) + 1) % 3
+  if throw == 0
+    puts "#{throw} case 3 or 6 : snow-house"
+  elsif throw == 1
+    puts "#{throw} case 1 or 4 : bridge"
+  else
+    puts "#{throw} case 2 or 5 : break a pillar"
+  end
+  puts bridge
+  bridge -= 1
+end
 
 
 =begin
